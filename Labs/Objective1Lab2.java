c8a301eb3d6a252ac9848003ae23d8602501d84f
Walter Valdez <waltersoso80@gmail.com>
2020-12-12 11:09:28 -0600

    finished Obj1Lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,21 @@
+/*
+Expected Output
+  >>  ----------------
+  >> | Grocery List   |
+  >> | 1. Milk        |
+  >> | 2. Eggs        |
+  >> | 3. Bread       |
+  >>  ----------------
+*/
+
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    // print out a grocery list like the expected output
+    System.out.println(" ---------------- ");
+    System.out.println("| Grocery List   |");
+    System.out.println("| 1. Milk        |");
+    System.out.println("| 2. Eggs        |");
+    System.out.println("| 3. Bread       |");
+    System.out.println(" ---------------- ");
+  }
+}
