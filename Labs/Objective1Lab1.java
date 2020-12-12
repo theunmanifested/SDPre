94c495ac0ad4e6919838b35e70c19ac973da27f7
Walter Valdez <waltersoso80@gmail.com>
2020-12-12 11:02:25 -0600

    Obj1Lab1 print completed

---
+++ Labs/Objective1Lab1.java
@@ -0,0 +1,6 @@
+public class Objective1Lab1 {
+  public static void main(String[] args) {
+    // add code that would print out the value "Hello, Skill Distillery"
+    System.out.println("Hello, Skill Distillery");
+  }
+}
