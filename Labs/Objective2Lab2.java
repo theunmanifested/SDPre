c82570e13514b927402d79ecceeac14b4d5edf54
Walter Valdez <waltersoso80@gmail.com>
2020-12-12 16:48:52 -0600

    finished Obj2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,17 @@
+// print out your name, age and the state where you were born.
+// Change the values to reflect your own personal information.
+
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Walter Valdez";
+    int age = 40;
+    String state = "Texas";
+
+    System.out.println("Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+    // System.out.println(name);
+    // System.out.println(name);
+  }
+}
+
+// Expected Output
+// Name: Charles Peterson Age: 47 State: Arkansas
