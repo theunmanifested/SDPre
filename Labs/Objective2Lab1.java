4eb745785007beadc4cd4cd8e709e275271f7e07
Walter Valdez <waltersoso80@gmail.com>
2020-12-12 16:38:44 -0600

    finsihed Obj2Lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,8 @@
+// Using the Starter Code below add code to the commented area that would print out the value "Hello World" stored in the hello variable.
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+    System.out.println(hello);
+
+  }
+}
