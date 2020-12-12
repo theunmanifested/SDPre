efdcd2fd0cf3f0a97f0a926accc0576647df6bea
Walter Valdez <waltersoso80@gmail.com>
2020-12-12 17:01:15 -0600

    finished Obj2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,17 @@
+// Replace the question marks on the line declaring the variable myVariable;
+// Replace the asterisks with a different value than you used before.
+// Run your code, and see that your output came out as expected.
+// This is to show that you can change the value in a variable after it has been assigned a value.
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String myVariable = "Taco";
+    System.out.println(myVariable);
+    myVariable = "Hotdog";
+    System.out.println(myVariable);
+  }
+}
+// Expected Output
+// Assuming the question marks were replace with the word "Taco" and the asterisks were replaced with the word "Hotdog".
+//
+// Taco
+// Hotdog
