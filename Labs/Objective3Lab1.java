dec4a40ac3b2611e47320763f9f774527c6fb878
Walter Valdez <waltersoso80@gmail.com>
2020-12-13 10:08:45 -0600

    finished Obj3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,19 @@
+// Assign values to num1, num2, and num3.
+// Calculate the average of those numbers and store the resulting value into average.
+// Print out the calculated average.
+
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 98;
+    num2 = 89;
+    num3 = 70;
+    average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+  }
+}
+
+// Expected Output
+// This output would result from numbers 15,6,2
+//
+// 7.66666666667
