f9eb4597acc7af756df50e2c0bb6ca83b0bbbc16
Walter Valdez <waltersoso80@gmail.com>
2020-12-15 08:39:55 -0600

    finished Obj5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,20 @@
+// Create a class structure with the name Objective5Lab1.
+// Create a main method
+// Declare and initialize two int variables, num1 and num2, with appropriate values.
+
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    if(num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2);
+    }
+    else if(num2 > num1) {
+      System.out.println(num2 + " is greater than " + num1);
+    }
+  }
+}
+
+// Expected Output
+// 15 is greater than 5
