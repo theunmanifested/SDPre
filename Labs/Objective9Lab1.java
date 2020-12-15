fd54a64e08b04854a0618bd8a0c67000f7eecc11
Walter Valdez <waltersoso80@gmail.com>
2020-12-15 14:20:07 -0600

    finished Obj9Lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,13 @@
+// Call the provided printName method.
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+    printName();
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
+
+// Expected Output
+// Hello Doug
